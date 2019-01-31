fb1607d8fc5e33a1b95322371192801c21e33b43
Stephane Darcy SIMO MBA <stephanedarcy.simomba@epitech.eu>
2019-01-31 07:59:24 +0100

    Fix Firebase database read and write

--- app/src/main/java/fr/msg/simbaste/kotlinemessenger/NewMessageActivity.kt
+++ app/src/main/java/fr/msg/simbaste/kotlinemessenger/NewMessageActivity.kt
@@ -2,10 +2,17 @@ package fr.msg.simbaste.kotlinemessenger
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+import com.squareup.picasso.Picasso
 import com.xwray.groupie.GroupAdapter
 import com.xwray.groupie.Item
 import com.xwray.groupie.ViewHolder
 import kotlinx.android.synthetic.main.activity_new_message.*
+import kotlinx.android.synthetic.main.user_row_new_message.view.*
 
 class NewMessageActivity : AppCompatActivity() {
 
@@ -17,18 +24,40 @@ class NewMessageActivity : AppCompatActivity() {
 
         val adapter = GroupAdapter<ViewHolder>()
 
-        adapter.add(UserItem())
-        adapter.add(UserItem())
-        adapter.add(UserItem())
-
         recyclerview_newmessage.adapter = adapter
+        fetchUser()
+    }
+
+    private fun fetchUser() {
+        val ref = FirebaseDatabase.getInstance().getReference("/users")
+        ref.addListenerForSingleValueEvent(object : ValueEventListener {
+            override fun onDataChange(p0: DataSnapshot) {
+                val adapter = GroupAdapter<ViewHolder>()
+                p0.children.forEach {
+                    Log.d("NewMessage", it.toString())
+                    val user = it.getValue(User::class.java)
+                    if (user != null) {
+                        adapter.add(UserItem(user))
+                    }
+                }
+                recyclerview_newmessage.adapter = adapter
+            }
+
+            override fun onCancelled(p0: DatabaseError) {
+
+            }
+        })
     }
 }
 
-class UserItem: Item<ViewHolder>() {
+class UserItem(val user: User): Item<ViewHolder>() {
 
     override fun bind(viewHolder: ViewHolder, position: Int) {
+        viewHolder.itemView.username_textview_new_message.text = user.username
 
+        Picasso.get()
+            .load(user.profileImageUrl)
+            .into(viewHolder.itemView.imageview_new_message_row)
     }
 
     override fun getLayout(): Int {
